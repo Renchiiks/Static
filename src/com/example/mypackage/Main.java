8983ba5fad2b4d02db4cf8cfc4fdb9245d0fb923
Renchiiks <renate.kalmikova@gmail.com>
2020-09-17 15:16:29 +0300

    Initial commit

---
+++ src/com/example/mypackage/Main.java
@@ -0,0 +1,29 @@
+package com.example.mypackage;
+
+public class Main {
+
+    public static int multiplier = 7;
+
+    public static void main(String[] args) {
+//        StaticTest firstInstance = new StaticTest("First Instance");
+//        System.out.println(firstInstance.getName() + " is instance number " + StaticTest.getNumInstances());
+//
+//        StaticTest secondInstance = new StaticTest("Second Instance");
+//        System.out.println(secondInstance.getName()+" is instance number " + secondInstance.getNumInstances());
+//
+//        StaticTest thirdInstance = new StaticTest("Second Instance");
+//
+//       System.out.println(secondInstance.getName()+" is instance number " + secondInstance.getNumInstances());
+
+
+        int answer = multiply(6);
+        System.out.println("The answer is " + answer);
+
+        System.out.println("Multiplier is " + multiplier);
+    }
+
+    public static int multiply(int number) {
+        return number * multiplier;
+    }
+
+}
